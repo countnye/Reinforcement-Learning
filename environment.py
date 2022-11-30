5cf31fb282507cb639287e93f1d866d1329bdbd3
countnye <91778509+countnye@users.noreply.github.com>
2022-11-30 18:07:05 +0100

    whatever i tried to debug

--- environment.py
+++ environment.py
@@ -4,12 +4,15 @@ import bandit as bb
 
 
 class Environment:
-    def __init__(self, t, n, k, bandit_type):
+    def __init__(self, time, t, n, k, bandit_type):
         # define the number of iterations.
         # Is also the time
-        self.T = t
+        self.time = time
         # define the number of bandits
         self.N = n
+        # define the number of times the 
+        # experiment should be run for
+        self.T = t
         # create N bandits with k arms of given type
         if bandit_type == 'b':
             self.bandits = [bb.Bandit(k, bb.Type.BERNOULLI) for _ in range(self.N)]
@@ -18,7 +21,7 @@ class Environment:
 
     # agent picks random arms (to check agent implementation)
     def random_strategy(self):
-        for _ in range(self.T):
+        for _ in range(self.time):
             for idx, bandit in enumerate(self.bandits):
                 chosen_arm = np.random.choice(bandit.k)
                 bandit.chooseArm(chosen_arm)
@@ -26,7 +29,7 @@ class Environment:
 
     # function for greedy strategy
     def greedy(self):
-        for _ in range(self.T):
+        for _ in range(self.time):
             for bandit in self.bandits:
                 # for every arm, calculate the action value
                 action_value = bandit.q_t()
@@ -37,7 +40,7 @@ class Environment:
 
     # function for epsilon greedy strategy
     def e_greedy(self, e):
-        for _ in range(self.T):
+        for _ in range(self.time):
             for bandit in self.bandits:
                 # explore e% of iterations
                 if np.random.random() > e:
@@ -63,8 +66,9 @@ class Environment:
     def optimistic(self):
         # starts by assigning all actions an initial value greater than
         # the mean reward we expect to receive after pulling each arm
-        for _ in range(self.T):
+        for _ in range(self.time):
             # Initialise high action values for all bandits
+            # NEED TO IMPLEMENT initQ0()
             map(lambda x: x.initQ0(), self.bandits)
             # Execute greedy strategy for bandits
             for bandit in self.bandits:
@@ -77,16 +81,21 @@ class Environment:
 
     # function for UCB strategy
     def UCB(self, c):
-        for t in range(self.T):
+        for t in range(1, self.time):
             for bandit in self.bandits:
                 q_t = bandit.q_t()
+                a_t = []
+                # For each action, calculate a_t
                 # n_a: number of times all actions were selected
-                n_a = bandit.n_a()
-                # list of all actions and their calculated
-                # confidence intervals
-                a_t = q_t + c * (np.sqrt(np.log(t) / n_a))
-                chosen_arm = a_t.index(max(a_t))
+                for action in range(bandit.k):
+                    n_a = bandit.n_a(action)
+                    # list of all actions and their calculated
+                    # confidence intervals
+                    a_t.append(q_t[action] + c * (np.sqrt(np.log(t) / n_a)))
                 # choose arm with largest UCB
+                print("Q_T: ", q_t, "A_T: ", a_t)
+                chosen_arm = a_t.index(max(a_t))
+                print("chosen arm: ", chosen_arm)
                 bandit.chooseArm(chosen_arm)
 
     # function for action preferences strategy
@@ -95,9 +104,10 @@ class Environment:
 
 
 # initialize the environment
-env = Environment(t=10, n=2, k=3, bandit_type='g')
+env = Environment(time=10, n=2, t=100, k=3, bandit_type='g')
 # execute the random strategy
-env.UCB(0.1)
+# env.UCB(0.1)
+env.e_greedy(0.1)
 # print arm count and rewards for each bandit
 print("FINAL RESULTS:")
 for num, agent in enumerate(env.bandits):
