3b67e2df66c957f6a100d2992dd021a81d0161c1
countnye <91778509+countnye@users.noreply.github.com>
2023-01-22 16:38:40 +0100

    set state space

--- StateSpace.py
+++ StateSpace.py
@@ -1,16 +1,45 @@
+import chess
+
+'''''
+
+Might need to copy the board to prevent breaking. Keyword: might
+There's an inbuilt copy function in the chess module.
+
+'''''
+
 class StateSpace:
     def __init__(self, board):
-        self.board = board
+        self.board = board.copy()
         self.state_space = {}
         self.set_state_space()
 
     # function to initialize the state space
-    def set_state_space(self):
-        state_space = {}
+    def set_state_space_rec(self):
         # create a function that takes self.board and applies all possible legal
         # moves and stores all those new board in the coordinate representation.
         # check for repeating boards and when no new board is possible, end loop.
-        self.state_space = state_space
+
+        # state space should be a dict in a dict in form {'state': {'action': q-value} }
+        if self.board.is_game_over():
+            return
+
+
+        for move in self.board.get_legal_moves():
+            self.board.push(move)
+            self.set_state_space_rec()
+            action = {}
+            # get FEN representation of board
+            state = self.board.fen()
+            # if state is not already in dict, add it
+            # and init q-value to 0
+            if state not in self.state_space.keys():
+                self.state_space[state] = {move : 0}
+            else:
+
+            self.board.pop()
+
+
+
 
     # function to get state space
     def get_state_space(self):
